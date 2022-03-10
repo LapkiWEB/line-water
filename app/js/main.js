43a5ab0c7c7ca7e6a82691f51303f22666b72832
Stepan-pc <Stefan.irk96@mail.ru>
2022-03-10 16:38:19 +0800

    add header + mobil, add section screen + adaptive

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('.menu__btn').on('click', (function () {
+    if (!$('.menu').hasClass('active')) {
+      $('.menu').addClass('active');
+      $('.menu__btn').addClass('active');
+      $('html, body').css('overflow', 'hidden');
+    } else {
+      $('.menu').removeClass('active');
+      $('.menu__btn').removeClass('active');
+      $('html, body').css('overflow', 'hidden');
+    }
+  }));
+})
