2e034a9b48500e589b90f87c7b1735acebb5bd9b
Stepan-pc <Stefan.irk96@mail.ru>
2022-04-25 16:43:05 +0800

    fix btn in section feedback, fix link in section footer

--- app/js/main.js
+++ app/js/main.js
@@ -15,14 +15,13 @@ $(function () {
   }));
 
 
-
-
-
   $('.reviews__slider__inner').slick({
     prevArrow: document.querySelector('.slick-prev'),
     nextArrow: document.querySelector('.slick-next'),
     infinite: true,
     speed: 1000,
+    fade: true,
+    cssEase: 'linear',
     slidesToShow: 1,
     slidesToScroll: 1,
     responsive: [{
@@ -40,20 +39,3 @@ $(function () {
 })
 
 
-
-let inputs = document.querySelectorAll('.feedback__file');
-Array.prototype.forEach.call(inputs, function (input) {
-  let label = input.nextElementSibling,
-    labelVal = label.querySelector('.input__file-button-text').innerText;
-
-  input.addEventListener('change', function (e) {
-    let countFiles = '';
-    if (this.files && this.files.length >= 1)
-      countFiles = this.files.length;
-
-    if (countFiles)
-      label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
-    else
-      label.querySelector('.input__file-button-text').innerText = labelVal;
-  });
-});
